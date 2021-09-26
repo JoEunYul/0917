0e8d7d111cc8d26e63858ca311b0ede8f1d4a1bc
JoEunYul <tjdqhd0464@naver.com>
2021-09-26 11:45:45 +0900

    cpp 변경

--- Newmain.cpp
+++ Newmain.cpp
@@ -4,20 +4,57 @@ using namespace std;
 
 int main()
 {
-	int First = 0;
-	int Second = 0;
+
+	/*
+	type
+	int -> float
+	float -> int
+	float <- doible
+	casting(형변환)
+	(형)원래형태
+	static_cast<새로운형> (원래 형태)
+	int -> char
+	char -> int
+	*/
+
+	unsigned int;
+
+
+	int Math = 0;
+	int Lang = 0;
+	int Engl = 0;
 	int Sum = 0;
+    
 
-	cout << " 첫번재 숫자를 입력해주세요. : ";
-	cin >> First;
-	cout << " 두번재 숫자를 입력해주세요. : ";
-	cin >> Second;
+	char A = 65;
+	int B = 'B';
 
-	Sum = First + Second;
+	int Sum = A + B;
 
-	cout << " 첫번재와 두번째 숫자를 합친 값 : " << Sum;
+	cout << static_cast<int>(Sum) << endl;
+
+	//cout << " 수학 점수를 입력하세요 : ";
+	//cin >> Math;
+	//cout << " 언어 점수를 입력하세요 : ";
+	//cin >> Lang;
+	//cout << " 영어 점수를 입력하세요 : ";
+	//cin >> Engl;
+
+	//Sum = Math + Lang + Engl;
+	//float Avg = static_cast<float>(Sum) / 3.0f;
+
+	//cout << "3과목의 점수의 합은 " << Sum << " 이며, 평균값은 " << Avg << " 입니다. " << endl;
+
+
+	char C = 512;
+	cout << C << endl;
+
+
+
+
+
 
 
 	return 0;
-
+	// 합계점수 ,평균점수
 }
